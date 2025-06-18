b21239762b4b9156397827ae3716ddf20bbabbb7
omar115 <omarhasan115@gmail.com>
2025-06-18 13:31:58 +0900

    prod bug fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import dj_database_url
+# import dj_database_url
 import os
 from pathlib import Path
 
